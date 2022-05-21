849c7079b719a57dbfe3bbcaf5173d90a6423956
MoritzB112 <moritzbaader@gmail.com>
2022-05-21 16:43:49 +0200

    Cambios esenciales de la estructura del proyecto

---
+++ DigitalSolutions/DigitalSolutions-jpa/src/main/java/es/uma/proyecto/jpa/Main.java
@@ -0,0 +1,21 @@
+package es.uma.proyecto.jpa;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("proyecto-ejb");
+		EntityManager em = emf.createEntityManager();
+		
+		
+		em.close();
+		emf.close();
+	}
+
+}
