ac7548d3fa6950ffc3fd34d738c6bb1148ff65c3
franVR10 <0619994495@uma.es>
2022-05-23 00:37:55 +0200

    prueba2

--- DigitalSolutions/DigitalSolutions-ejb/src/main/java/es/uma/proyecto/ejb/InicializaBBDD.java
+++ DigitalSolutions/DigitalSolutions-ejb/src/main/java/es/uma/proyecto/ejb/InicializaBBDD.java
@@ -10,6 +10,7 @@ import javax.ejb.Startup;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import es.uma.proyecto.ejb.Excepciones.PasswordException;
 import es.uma.proyecto.jpa.Usuario;
 
 @Singleton
@@ -33,7 +34,7 @@ public class InicializaBBDD {
 		u.setCorreo("testU@test.test");
 		u.setEsAdministrativo(false);
 		u.setSalt(getSalt());
-		u.setPassword(hashPasswword(u.getSalt(), "test"));
+		u.setPassword(hashPassword(u.getSalt(), "test"));
 		u.setUsuario("testU");
 		em.persist(u);
 		
@@ -41,19 +42,21 @@ public class InicializaBBDD {
         a.setCorreo("testU@admin.test");
         a.setEsAdministrativo(true);
         a.setSalt(getSalt());
-        a.setPassword(hashPasswword(a.getSalt(), "admin"));
+        a.setPassword(hashPassword(a.getSalt(), "admin"));
         a.setUsuario("admin");
         em.persist(a);
 
 	}
 	
-	private String hashPasswword(String salt, String password) {
+	private String hashPassword(String password, String salt){
 		try {
+			String encodedSalt = Base64.getEncoder().encodeToString(salt.getBytes());
 			MessageDigest mg = MessageDigest.getInstance("SHA-256");
-			mg.update(Base64.getDecoder().decode(salt));
-			byte[] hashed=mg.digest(Base64.getDecoder().decode(password));
+			mg.update(Base64.getDecoder().decode(encodedSalt));
+			String encodedString = Base64.getEncoder().encodeToString(password.getBytes());
+			byte[] hashed=mg.digest(Base64.getDecoder().decode(encodedString));
 			
-			return Base64.getEncoder().encodeToString(hashed);
+			return new String(hashed);
 
 		} catch (Exception e) {
 			return e.getMessage();
