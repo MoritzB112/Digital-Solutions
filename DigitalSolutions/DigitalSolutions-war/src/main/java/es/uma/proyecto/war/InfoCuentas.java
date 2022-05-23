903c2d15a40d4dbb80755062e4bf5bc39dcf894e
MoritzB112 <0619977862@uma.es>
2022-05-23 21:13:19 +0200

    Modificaciones en cuanta

--- DigitalSolutions/DigitalSolutions-war/src/main/java/es/uma/proyecto/war/InfoCuentas.java
+++ DigitalSolutions/DigitalSolutions-war/src/main/java/es/uma/proyecto/war/InfoCuentas.java
@@ -9,7 +9,11 @@ import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import es.uma.proyecto.jpa.Cuenta_Fintech;
+import es.uma.proyecto.jpa.Cuenta_Referencia;
 import es.uma.proyecto.jpa.Empresa;
+import es.uma.proyecto.jpa.Pooled_Account;
+import es.uma.proyecto.jpa.Segregada;
 
 @Named(value = "infoCuentas")
 @RequestScoped
@@ -28,5 +32,30 @@ public class InfoCuentas implements Serializable {
 		sesion.setEm(em);
 		
 		return "clientView.xhtml";
+	}
+	
+	public String tipoCuenta(Cuenta_Fintech cf) {
+		if(esSegregada(cf)) {
+			return "Segregada";
+		}else if(esPooled(cf)) {
+			return "Pooled_Account";
+		}
+		return null;
+	}
+	
+	public boolean esSegregada(Cuenta_Fintech cf) {
+		return cf instanceof Segregada;
+	}
+	
+	public boolean esPooled(Cuenta_Fintech cf) {
+		return cf instanceof Pooled_Account;
+	}
+	
+	public String sacarSaldo(Cuenta_Fintech cf) {
+		if(esSegregada(cf)) {
+			Cuenta_Referencia cr=((Segregada)cf).getCr();
+			return cr.getSaldo().toString()+" "+cr.getDiv().getAbreviatura();
+		}
+		return "No disponible";
 	}
 }
