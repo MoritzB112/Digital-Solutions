e36a3b67c3433fa20cce77d11536cffd5122b367
MoritzB112 <0619977862@uma.es>
2022-05-23 16:07:55 +0200

    Cambios inizializarbd

--- DigitalSolutions/DigitalSolutions-ejb/src/main/java/es/uma/proyecto/ejb/InicializaBBDD.java
+++ DigitalSolutions/DigitalSolutions-ejb/src/main/java/es/uma/proyecto/ejb/InicializaBBDD.java
@@ -1,5 +1,6 @@
 package es.uma.proyecto.ejb;
 
+import java.security.MessageDigest;
 import java.security.SecureRandom;
 import java.util.Date;
 
@@ -18,92 +19,119 @@ import es.uma.proyecto.jpa.Usuario;
 @Singleton
 @Startup
 public class InicializaBBDD {
-	
+
 	private static SecureRandom sm = new SecureRandom();
 
 	@PersistenceContext(name = "proyecto-ejb")
 	private EntityManager em;
-	
+
 	@PostConstruct
 	public void inicializar() {
-		
 
-		if(em.find(Usuario.class, "testU")!=null) {
+		if (em.find(Usuario.class, "testU") != null) {
 			return;
 		}
-		
-		Usuario u=new Usuario();
+
+		Usuario u = new Usuario();
 		u.setCorreo("testU@test.test");
 		u.setEsAdministrativo(false);
-//		u.setSalt(getSalt());
-//		u.setPassword(hashPasswword(u.getSalt(), "test"));
-		u.setPassword("test");
+		u.setSalt(parseString(getSalt()));
+		u.setPassword(parseString(hashPassword(parseByte(u.getSalt()), "test".getBytes())));
 		u.setUsuario("testU");
 		em.persist(u);
-		
 
-		Usuario a=new Usuario();
-        a.setCorreo("testU@admin.test");
-        a.setEsAdministrativo(true);
-//        a.setSalt(getSalt());
-//        a.setPassword(hashPasswword(a.getSalt(), "admin"));
-        u.setPassword("test");
-        a.setUsuario("admin");
-        em.persist(a);
-        
-        Persona_Autorizada pa = new Persona_Autorizada();
-        pa.setID(1L);
-        pa.setNombre("alguno");
-        pa.setApellidos("garcia");
-        pa.setDireccion("pozo");
-        pa.setCiudad("malaga");
-        pa.setCodigoPostal(29010);
-        pa.setUs(u);
-        pa.setPais("España");
-        em.persist(pa);
-        
-        Empresa emp = new Empresa();
-        emp.setIdentificacion("algunString");
-        emp.setId(1L);
-        emp.setTipo_cliente("juridica");
-        emp.setEstado("activa");
-        emp.setFecha_alta(new Date());
-        emp.setDireccion("direccion");
-        emp.setCiudad("granada");
-        emp.setCodigoPostal(29000);
-        emp.setPais("España");
-        emp.setRazon_social("trabajador");
-        em.persist(emp);
-        
-        Autorizacion au = new Autorizacion();
-        Autorizacion_PK aupk = new Autorizacion_PK();
-        aupk.setEmID(1L);
-        aupk.setPaID(1L);
-        au.setId(aupk);
-        au.setTipo("prueba");
-        au.setEm(emp);
-        au.setPa(pa);
-        em.persist(au);
+		Usuario a = new Usuario();
+		a.setCorreo("testU@admin.test");
+		a.setEsAdministrativo(true);
+		a.setSalt(parseString(getSalt()));
+		a.setPassword(parseString(hashPassword(parseByte(a.getSalt()), "test".getBytes())));
+		a.setUsuario("admin");
+		em.persist(a);
+
+		Persona_Autorizada pa = new Persona_Autorizada();
+		pa.setID(1L);
+		pa.setNombre("alguno");
+		pa.setApellidos("garcia");
+		pa.setDireccion("pozo");
+		pa.setCiudad("malaga");
+		pa.setCodigoPostal(29010);
+		pa.setUs(u);
+		pa.setPais("España");
+		em.persist(pa);
+
+		Empresa emp = new Empresa();
+		emp.setIdentificacion("algunString");
+		emp.setId(1L);
+		emp.setTipo_cliente("juridica");
+		emp.setEstado("activa");
+		emp.setFecha_alta(new Date());
+		emp.setDireccion("direccion");
+		emp.setCiudad("granada");
+		emp.setCodigoPostal(29000);
+		emp.setPais("España");
+		emp.setRazon_social("trabajador");
+		em.persist(emp);
+
+		Autorizacion au = new Autorizacion();
+		Autorizacion_PK aupk = new Autorizacion_PK();
+		aupk.setEmID(1L);
+		aupk.setPaID(1L);
+		au.setId(aupk);
+		au.setTipo("prueba");
+		au.setEm(emp);
+		au.setPa(pa);
+		em.persist(au);
 	}
-	
-//	private byte[] hashPasswword(byte[] salt, String password) {
-//		try {
-//			MessageDigest mg = MessageDigest.getInstance("SHA-256");
-//			mg.update(salt);
-//			byte[] hashed=mg.digest(password.getBytes());
-//			
-//			return hashed;
-//
-//		} catch (Exception e) {
-//			return null;
-//		}
-//	}
-//	
-//	private byte[] getSalt() {
-//		byte[] salt = new byte[16];
-//		sm.nextBytes(salt);
-//		
-//		return salt;
-//	}
+
+	private byte[] hashPassword(byte[] salt, byte[] password) {
+		try {
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+			digest.update(salt);
+			return digest.digest(password);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return null;
+	}
+
+	private boolean comprobarPw(Usuario u, String pw) {
+		try {
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+			digest.update(parseByte(u.getSalt()));
+			return MessageDigest.isEqual(parseByte(u.getPassword()), digest.digest(pw.getBytes()));
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return false;
+	}
+
+	private String parseString(byte[] ba) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < ba.length; i++) {
+			sb.append(Integer.toString((ba[i] & 0xff) + 0x100, 16).substring(1));
+		}
+
+		return sb.toString();
+	}
+
+	private byte[] parseByte(String str) {
+		byte[] ans = new byte[str.length() / 2];
+		for (int i = 0; i < ans.length; i++) {
+			int index = i * 2;
+			int val = Integer.parseInt(str.substring(index, index + 2), 16);
+			ans[i] = (byte) val;
+		}
+
+		return ans;
+	}
+
+	private byte[] getSalt() {
+		byte[] salt = new byte[16];
+		sm.nextBytes(salt);
+
+		return salt;
+	}
 
 }
